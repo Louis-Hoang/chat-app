872860362982980a8a7948cde465418d71eefcb4
Louis-Hoang <hoang_t1@denison.edu>
2023-01-05 16:37:47 -0500

    form create

--- application.py
+++ application.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from wtform_fields import *
+
 
 app = Flask(__name__)
 app.secret_key='placeholder'
 
 @app.route("/", methods=["GET","POST"] )
 def index():
-    return render_template("index.html")
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Good"
+    return render_template("index.html", form = reg_form)
 
 if __name__ == "__main__": #allow excute when file run as script
     app.run(debug=True)
