0a82429067d0a9cbf331a77c785e98c06f6dfe5b
Louis-Hoang <hoang_t1@denison.edu>
2023-01-05 23:54:42 -0500

    Registration Verify

--- application.py
+++ application.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template
 from wtform_fields import *
-
+from models import *
 
 app = Flask(__name__)
-app.secret_key='placeholder'
+app.secret_key = 'placeholder'
 
-@app.route("/", methods=["GET","POST"] )
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://okenthdbfaisxz:bcc6d85268ee971c63712f50b1f39ff76f105e7c71233322a3e665673dad5836@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d6bhbpbas2bvci"
+
+db = SQLAlchemy(app)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
-        return "Good"
-    return render_template("index.html", form = reg_form)
+        username = reg_form.username.data
+        password = reg_form.password.data
 
-if __name__ == "__main__": #allow excute when file run as script
+        # Check uniqueness
+
+        # Add user to DB
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "User info added"
+
+    return render_template("index.html", form=reg_form)
+
+
+if __name__ == "__main__":  # allow excute when file run as script
     app.run(debug=True)
--- wtform_fields.py
+++ wtform_fields.py
@@ -1,18 +1,25 @@
-from flask_wtf import FlaskForm 
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from models import *
+
 
 class RegistrationForm (FlaskForm):
-    username = StringField('username_label', 
-    validators =[InputRequired(message="Username required"), 
-    Length(min=4, max=25, message="Username must be between 4 and 25 characters")] )
-   
-    password = PasswordField('password_label', 
-    validators =[InputRequired(message="Password required"), 
-    Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
-    
-    confirm_pswd = PasswordField('confirm_pswd_label',
-    validators =[InputRequired(message="Password required"), 
-    EqualTo('password', message="Password must match")])
+    username = StringField('username_label',
+                           validators=[InputRequired(message="Username required"),
+                                       Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
 
-    submit_button=SubmitField('Create')
+    password = PasswordField('password_label',
+                             validators=[InputRequired(message="Password required"),
+                                         Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+
+    confirm_pswd = PasswordField('confirm_pswd_label',
+                                 validators=[InputRequired(message="Password required"),
+                                             EqualTo('password', message="Password must match")])
+
+    submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists.")
