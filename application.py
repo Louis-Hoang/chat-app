4a0a048703e616fca26a0f66cdd5f15a6f97b3a8
Louis-Hoang <hoang_t1@denison.edu>
2023-01-06 12:03:22 -0500

    log in log out reroute

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from wtform_fields import *
 from models import *
 
@@ -9,6 +10,14 @@ app.secret_key = 'placeholder'
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://okenthdbfaisxz:bcc6d85268ee971c63712f50b1f39ff76f105e7c71233322a3e665673dad5836@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d6bhbpbas2bvci"
 
 db = SQLAlchemy(app)
+
+login = LoginManager(app)
+login.init_app(app)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -34,9 +43,24 @@ def index():
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        return "Logged in"
-
+        user_object = User.query.filter_by(
+            username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for('chat'))
     return render_template("login.html", form=login_form)
+
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if not current_user.is_authenticated:
+        return "Please log in first"
+    return "let's started !"
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    logout_user()
+    return "Log out success"
 
 
 if __name__ == "__main__":  # allow excute when file run as script
