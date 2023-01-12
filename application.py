14ac7776f5a357c8e28ae30c0cc30f1b66a9e461
Louis-Hoang <hoang_t1@denison.edu>
2023-01-12 18:20:50 -0500

    Config link to database

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET')
 
 # Config database
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE')
 db = SQLAlchemy(app)
 
 # Init SocketIO
